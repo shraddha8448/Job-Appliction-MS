12d48546ba1657f62cfab3f56cd27a6f65506bdd
Shraddha Jadhav <shraddhajadhav191201@gmail.com>
2024-11-28 17:56:37 +0530

    Job service

---
+++ service-regestry/src/test/java/com/service_reg/service_regestry/ServiceRegestryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service_reg.service_regestry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegestryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
